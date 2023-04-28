bc93a8f0a8f32a3160250c47d7b38b21f8f7251b
kp54 <kangpang65@gmail.com>
2023-04-28 23:10:28 +0900

    tweak configurations

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,9 @@ import { defineConfig } from "vite";
 import { viteSingleFile } from "vite-plugin-singlefile";
 
 export default defineConfig({
-  plugins: [viteSingleFile()],
+  plugins: [
+    viteSingleFile({
+      removeViteModuleLoader: true,
+    }),
+  ],
 });
