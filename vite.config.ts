b2b25f35aa64c06fd86dda38e0b40946cb65044b
kp54 <kangpang65@gmail.com>
2024-08-12 15:16:54 +0900

    Revert "minify built assets"
    This reverts commit c658b00b240f37df2dbcf84de0b808166b46d6a2.

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,8 @@ import { viteSingleFile } from "vite-plugin-singlefile";
 
 export default defineConfig({
   plugins: [
-    viteSingleFile(),
+    viteSingleFile({
+      removeViteModuleLoader: true,
+    }),
   ],
-  build: {
-    target: 'esnext',
-    modulePreload: false,
-  },
 });
